41f644ef335f133a49e2a3d3db4d2e475c77d985
asbjosh <asbjornsh1@gmail.com>
2025-01-27 11:40:07 +0100

    feat: added sql connection

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Sydvest_Bo;
+
+class Program
+{
+    public static string connectionString = @"Data Source=AsbLaptop\SQLEXPRESS;Initial Catalog=testbase;Integrated Security=True;Trust Server Certificate=True";
+    static void Main(string[] args)
+    {
+        Connect();
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+    static void Connect()
+    {
+        try
+        {
+            SqlConnection cnn;
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            Console.WriteLine("Connection True");
+            cnn.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
