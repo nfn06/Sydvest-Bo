5b9fffa627a00d64e56f66a52bd7afdbbc92f903
fancy-11 <fancypantsbagel@gmail.com>
2025-01-28 13:08:29 +0100

    feat: added abstract class ItemManager

---
+++ ItemManager.cs
@@ -0,0 +1,12 @@
+﻿namespace Sydvest_Bo;
+
+public abstract class ItemManager
+{
+    internal abstract void Main();
+    internal abstract void PrintMenu();
+    internal abstract void PrintItems();
+    internal abstract void GetAll();
+    internal abstract void Add();
+    internal abstract void Update();
+    internal abstract void Delete();
+}
