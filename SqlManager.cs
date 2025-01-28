f7c4078882c1f154aec43ff2217ba53865bad2b1
asbjosh <asbjornsh1@gmail.com>
2025-01-28 14:14:14 +0100

    feat: CRUD through SqlManager

--- SqlManager.cs
+++ SqlManager.cs
@@ -1,23 +1,55 @@
 ﻿using Microsoft.Data.SqlClient;
 
-namespace Sydvest_Bo;
-
-public class SqlManager
+namespace Sydvest_Bo
 {
-    public static string connectionString = @"Data Source=AsbLaptop\SQLEXPRESS;Initial Catalog=sbdb;Integrated Security=True;Trust Server Certificate=True";
-    public static void Connect()
+    public class SqlManager
     {
-        try
+        public static string connectionString = @"Data Source=AsbLaptop\SQLEXPRESS;Initial Catalog=sbdb;Integrated Security=True;Trust Server Certificate=True";
+
+        public static void Connect()
         {
-            using (SqlConnection cnn = new SqlConnection(connectionString))
+            try
             {
-                cnn.Open();
-                Console.WriteLine("Connection True");
+                using (SqlConnection cnn = new SqlConnection(connectionString))
+                {
+                    cnn.Open();
+                    Console.WriteLine("Connection Successful");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
             }
         }
-        catch (Exception ex)
+
+        public static void Add(string tableName, string columns, string values)
         {
-            Console.WriteLine($"Error: {ex.Message}");
+            string query = $"INSERT INTO {tableName} ({columns}) VALUES ({values})";
+            ExecuteQuery(query);
+        }
+
+        public static void Update(string tableName, string setClause, string condition)
+        {
+            string query = $"UPDATE {tableName} SET {setClause} WHERE {condition}";
+            ExecuteQuery(query);
+        }
+
+        public static void Delete(string tableName, string condition)
+        {
+            string query = $"DELETE FROM {tableName} WHERE {condition}";
+            ExecuteQuery(query);
+        }
+
+        private static void ExecuteQuery(string query)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
