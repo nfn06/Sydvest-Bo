16f02f39d5324f894501aa9ef16593acbfbda4c1
nfn06 <noahfnielsen11@gmail.com>
2025-01-28 14:00:42 +0100

    feat: added OwnerManager class

---
+++ OwnerManager.cs
@@ -0,0 +1,39 @@
+﻿namespace Sydvest_Bo;
+
+public class OwnerManager : ItemManager
+{
+    internal override void Main()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal override void PrintMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal override void GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal override void PrintItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal override void Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal override void Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal override void Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Pagination.cs
+++ Pagination.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 namespace Sydvest_Bo;
 
 public class Pagination
