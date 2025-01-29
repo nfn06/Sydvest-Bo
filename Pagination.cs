c14043c7bb61299ec7e6348aac73e506dfa69249
asbjosh <asbjornsh1@gmail.com>
2025-01-29 12:45:36 +0100

    feat: added indexes to owner menu

--- Pagination.cs
+++ Pagination.cs
@@ -1,35 +1,36 @@
-﻿using Microsoft.Data.SqlClient;
-
-namespace Sydvest_Bo
+﻿namespace Sydvest_Bo;
+public class Pagination
 {
-    public class Pagination
+    public (List<string> results, bool hasMore) GetPaginatedResults(string tableName, string columnName, int pageNumber, int pageSize)
     {
-        public (List<string> results, bool hasMore) GetPaginatedResults(string tableName, string columnName, int pageNumber, int pageSize)
+        if (pageNumber < 1)
+            pageNumber = 1;
+
+        List<string> results;
+        bool hasMore;
+
+        string query = $@"
+            SELECT {columnName} 
+            FROM {tableName}
+            ORDER BY Id
+            OFFSET @Offset ROWS 
+            FETCH NEXT @PageSize ROWS ONLY;";
+
+        var parameters = new Dictionary<string, object>
         {
-            if (pageNumber < 1)
-                pageNumber = 1;
+            { "@Offset", (pageNumber - 1) * pageSize },
+            { "@PageSize", pageSize }
+        };
 
-            List<string> results;
-            bool hasMore;
+        results = SqlManager.ExecuteQuery(query, parameters, columnName);
 
-            string query = $@"
-                SELECT {columnName} 
-                FROM {tableName}
-                ORDER BY Id
-                OFFSET @Offset ROWS 
-                FETCH NEXT @PageSize ROWS ONLY;";
-
-            var parameters = new Dictionary<string, object>
-            {
-                { "@Offset", (pageNumber - 1) * pageSize },
-                { "@PageSize", pageSize }
-            };
-
-            results = SqlManager.ExecuteQuery(query, parameters, columnName);
-
-            hasMore = results.Count == pageSize;
-
-            return (results, hasMore);
+        for (int i = 0; i < results.Count; i++)
+        {
+            results[i] = $"{((pageNumber - 1) * pageSize) + i + 1}. {results[i]}";
         }
+
+        hasMore = results.Count == pageSize;
+
+        return (results, hasMore);
     }
 }
